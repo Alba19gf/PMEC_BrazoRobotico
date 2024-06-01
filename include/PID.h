20f2f1bdbac4b4f1b110b0704fab5887f36d145a
Froylán Aragón <62692906+Faragoz@users.noreply.github.com>
2024-06-01 20:18:20 +0200

    Commit errors

--- include/PID.h
+++ include/PID.h
@@ -9,15 +9,12 @@ class PID
         PID();
         PID(float Kp, float Ki, float Kd);
         PID(float Kp, float Ki, float Kd, float dt, float max, float min);
-<<<<<<< HEAD
         int setKP(float Kp);
         int setKI(float Ki);
         int setKD(float Kd);
         int setdt(float dt);
         int setMax(float max);
         int setMin(float min);
-=======
->>>>>>> 0131d2e687cf24149704c2174ceff6295174a4da
         int setK(float Kp, float Ki, float Kd);
         int setP(float dt, float max, float min);
         float calc(float currentValue, float desiredValue);
@@ -27,7 +24,6 @@ class PID
         float _Kp;
         float _Ki;
         float _Kd;
-<<<<<<< HEAD
         float _dt = 0.1;
         float _max = 1.0;
         float _min = -1.0;
@@ -35,13 +31,6 @@ class PID
         float _sum_error = 0.0;
         float _maxVal = 180.0;
         float _minVal = 0.0;
-=======
-        float _dt;
-        float _max;
-        float _min;
-        float _pre_error = 0.0;
-        float _sum_error = 0.0;
->>>>>>> 0131d2e687cf24149704c2174ceff6295174a4da
 };
 
 #endif
