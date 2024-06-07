70e615638e488f1824b7ee94eb59ba0d68eef441
Alba19gf <115873114+Alba19gf@users.noreply.github.com>
2024-06-07 13:52:01 +0200

    matlab a c

--- src/main.cpp
+++ src/main.cpp
@@ -1,147 +1,162 @@
+#include <Arduino.h>
 #include <stdio.h>
-#include <stdbool.h>
 
-#define EMPTY 0
-#define PLAYER_X 1
-#define PLAYER_O 2
+#define SIZE 3
 
-void printBoard(int board[3][3]) {
-    for (int i = 0; i < 3; i++) {
-        for (int j = 0; j < 3; j++) {
-            if (board[i][j] == PLAYER_X) {
-                printf(" X ");
-            } else if (board[i][j] == PLAYER_O) {
-                printf(" O ");
-            } else {
-                printf(" . ");
-            }
+void convertir_tablero(int tablero_original[SIZE][SIZE], int tablero_convertido[SIZE][SIZE])
+ {
+    for (int i = 0; i < SIZE; i++) {
+        for (int j = 0; j < SIZE; j++) {
+            if (tablero_original[i][j] == 2)
+                tablero_convertido[i][j] = 0;
+            else if (tablero_original[i][j] == 0)
+                tablero_convertido[i][j] = -1;
+            else
+                tablero_convertido[i][j] = 1;
         }
-        printf("\n");
     }
 }
 
-int checkWinner(int board[3][3]) {
-    int winConditions[8][3][2] = {
-        {{0, 0}, {0, 1}, {0, 2}}, {{1, 0}, {1, 1}, {1, 2}}, {{2, 0}, {2, 1}, {2, 2}}, 
-        {{0, 0}, {1, 0}, {2, 0}}, {{0, 1}, {1, 1}, {2, 1}}, {{0, 2}, {1, 2}, {2, 2}}, 
-        {{0, 0}, {1, 1}, {2, 2}}, {{0, 2}, {1, 1}, {2, 0}}
+int comprueba_ganador(int tablero[SIZE][SIZE], int *empate)
+ {
+    int combinaciones[8][3][2] = {
+        {{0, 0}, {0, 1}, {0, 2}}, {{1, 0}, {1, 1}, {1, 2}}, {{2, 0}, {2, 1}, {2, 2}}, // filas
+        {{0, 0}, {1, 0}, {2, 0}}, {{0, 1}, {1, 1}, {2, 1}}, {{0, 2}, {1, 2}, {2, 2}}, // columnas
+        {{0, 0}, {1, 1}, {2, 2}}, {{0, 2}, {1, 1}, {2, 0}}  // diagonales
     };
 
+    *empate = 1;
     for (int i = 0; i < 8; i++) {
-        if (board[winConditions[i][0][0]][winConditions[i][0][1]] != EMPTY &&
-            board[winConditions[i][0][0]][winConditions[i][0][1]] == board[winConditions[i][1][0]][winConditions[i][1][1]] &&
-            board[winConditions[i][0][0]][winConditions[i][0][1]] == board[winConditions[i][2][0]][winConditions[i][2][1]]) {
-            return board[winConditions[i][0][0]][winConditions[i][0][1]];
+        int a = combinaciones[i][0][0], b = combinaciones[i][0][1];
+        int c = combinaciones[i][1][0], d = combinaciones[i][1][1];
+        int e = combinaciones[i][2][0], f = combinaciones[i][2][1];
+
+        if (tablero[a][b] != 0 && tablero[a][b] == tablero[c][d] && tablero[a][b] == tablero[e][f]) {
+            return tablero[a][b];
         }
     }
 
-    for (int i = 0; i < 3; i++) {
-        for (int j = 0; j < 3; j++) {
-            if (board[i][j] == EMPTY) {
-                return -1;
+    for (int i = 0; i < SIZE; i++) {
+        for (int j = 0; j < SIZE; j++) {
+            if (tablero[i][j] == 0) {
+                *empate = 0;
+                return 0;
             }
         }
     }
+
     return 0;
 }
 
-int minimax(int board[3][3], int depth, bool isMaximizing) {
-    int score = checkWinner(board);
-    if (score == PLAYER_X) return -10 + depth;
-    if (score == PLAYER_O) return 10 - depth;
-    if (score == 0) return 0;
+int minimax(int tablero[SIZE][SIZE], int prof, int isMax) 
+{
+    int empate;
+    int ganador = comprueba_ganador(tablero, &empate);
 
-    if (isMaximizing) {
-        int bestScore = -1000;
-        for (int i = 0; i < 3; i++) {
-            for (int j = 0; j < 3; j++) {
-                if (board[i][j] == EMPTY) {
-                    board[i][j] = PLAYER_O;
-                    int value = minimax(board, depth + 1, false);
-                    board[i][j] = EMPTY;
-                    bestScore = (value > bestScore) ? value : bestScore;
+    if (ganador != 0) {
+        return (ganador == 1 ? 100 - prof : -100 + prof);
+    } else if (empate) {
+        return 0;
+    }
+
+    if (isMax) {
+        int mejorPuntuacion = INT_MIN;
+        for (int i = 0; i < SIZE; i++) {
+            for (int j = 0; j < SIZE; j++) {
+                if (tablero[i][j] == 0) {
+                    tablero[i][j] = 1;
+                    int puntuacion = minimax(tablero, prof + 1, 0);
+                    tablero[i][j] = 0;
+                    if (puntuacion > mejorPuntuacion) {
+                        mejorPuntuacion = puntuacion;
+                    }
                 }
             }
         }
-        return bestScore;
+        return mejorPuntuacion;
     } else {
-        int bestScore = 1000;
-        for (int i = 0; i < 3; i++) {
-            for (int j = 0; j < 3; j++) {
-                if (board[i][j] == EMPTY) {
-                    board[i][j] = PLAYER_X;
-                    int value = minimax(board, depth + 1, true);
-                    board[i][j] = EMPTY;
-                    bestScore = (value < bestScore) ? value : bestScore;
+        int mejorPuntuacion = INT_MAX;
+        for (int i = 0; i < SIZE; i++) {
+            for (int j = 0; j < SIZE; j++) {
+                if (tablero[i][j] == 0) {
+                    tablero[i][j] = -1;
+                    int puntuacion = minimax(tablero, prof + 1, 1);
+                    tablero[i][j] = 0;
+                    if (puntuacion < mejorPuntuacion) {
+                        mejorPuntuacion = puntuacion;
+                    }
                 }
             }
         }
-        return bestScore;
+        return mejorPuntuacion;
     }
 }
 
-void findBestMove(int board[3][3], int *bestMoveX, int *bestMoveY) {
-    int bestScore = -1000;
-    *bestMoveX = -1;
-    *bestMoveY = -1;
+void mov_optimo(int tablero_convertido[SIZE][SIZE], int *fila, int *columna) 
+{
+    int mejor_puntuacion = INT_MIN;
+    int casilla_optima = -1;
 
-    for (int i = 0; i < 3; i++) {
-        for (int j = 0; j < 3; j++) {
-            if (board[i][j] == EMPTY) {
-                board[i][j] = PLAYER_O;
-                int moveScore = minimax(board, 0, false);
-                board[i][j] = EMPTY;
-                if (moveScore > bestScore) {
-                    bestScore = moveScore;
-                    *bestMoveX = i;
-                    *bestMoveY = j;
+    for (int i = 0; i < SIZE; i++) {
+        for (int j = 0; j < SIZE; j++) {
+            if (tablero_convertido[i][j] == 0) {
+                tablero_convertido[i][j] = 1;
+                int puntuacion = minimax(tablero_convertido, 0, 0);
+                tablero_convertido[i][j] = 0;
+
+                if (puntuacion > mejor_puntuacion) {
+                    mejor_puntuacion = puntuacion;
+                    *fila = i;
+                    *columna = j;
                 }
             }
         }
     }
 }
 
-int main() {
-    int board[3][3] = {
-        {0, 0, 0}, // Ejemplo de matriz de entrada del código de visión artificial
-        {0, 0, 0}, // Aquí deberías ingresar la matriz real proveniente del código de visión artificial
-        {0, 0, 0}
+void setup() {
+    Serial.begin(115200);
+
+    int tablero_original[SIZE][SIZE] = {
+        {-1, 2, -1},
+        {2, 1, 2},
+        {2, 1, 2}
     };
 
-    int player = PLAYER_X;
-    int bestMoveX, bestMoveY;
-    int winner;
+    int tablero_convertido[SIZE][SIZE];
+    convertir_tablero(tablero_original, tablero_convertido);
 
-    while ((winner = checkWinner(board)) == -1) {
-        if (player == PLAYER_X) {
-            int x, y;
-            printf("Introduce la fila (0-2): ");
-            scanf("%d", &x);
-            printf("Introduce la columna (0-2): ");
-            scanf("%d", &y);
-            if (board[x][y] == EMPTY) {
-                board[x][y] = PLAYER_X;
-                player = PLAYER_O;
-            } else {
-                printf("Movimiento no válido. Inténtalo de nuevo.\n");
-            }
-        } else {
-            findBestMove(board, &bestMoveX, &bestMoveY);
-            board[bestMoveX][bestMoveY] = PLAYER_O;
-            player = PLAYER_X;
-        }
+    int empate;
+    int ganador = comprueba_ganador(tablero_convertido, &empate);
 
-        printBoard(board);
-        printf("\n");
-    }
-
-    if (winner == PLAYER_X) {
-        printf("¡Has ganado!\n");
-    } else if (winner == PLAYER_O) {
-        printf("¡La IA ha ganado!\n");
+    if (ganador == 1) {
+        Serial.println("El robot ha ganado.");
+    } else if (ganador == -1) {
+        Serial.println("El jugador humano ha ganado.");
+    } else if (empate) {
+        Serial.println("El juego ha terminado en empate.");
     } else {
-        printf("¡Empate!\n");
-    }
+        int fila, columna;
+        mov_optimo(tablero_convertido, &fila, &columna);
+        tablero_convertido[fila][columna] = 1;
 
-    return 0;
+        ganador = comprueba_ganador(tablero_convertido, &empate);
+
+        Serial.print("La máquina debe hacer su próximo movimiento en la fila ");
+        Serial.print(fila);
+        Serial.print(" y columna ");
+        Serial.println(columna);
+
+        if (ganador == 1) {
+            Serial.println("El robot ha ganado con este movimiento.");
+        } else if (ganador == -1) {
+            Serial.println("El jugador humano ha ganado.");
+        } else if (empate) {
+            Serial.println("El juego ha terminado en empate con este movimiento.");
+        }
+    }
+}
+
+void loop() {
+    // Nada que hacer aquí
 }
