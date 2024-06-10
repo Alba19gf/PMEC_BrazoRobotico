6035a1c1cd54cd9ad99a0990760b69471034b08d
Alba19gf <115873114+Alba19gf@users.noreply.github.com>
2024-06-10 21:18:34 +0200

    recibir MatrizEstado

--- src/main.cpp
+++ src/main.cpp
@@ -1,57 +1,61 @@
 #include <Arduino.h>
 #include <stdio.h>
+#include <limits.h>  // Para INT_MIN y INT_MAX
 
 #define SIZE 3
 
-void convertir_tablero(int tablero_original[SIZE][SIZE], int tablero_convertido[SIZE][SIZE])
- {
+// Declarar la matriz global
+int MatrizEstado[SIZE][SIZE];
+
+// Declarar la función externa
+extern void Fn_MatrizState();
+
+void convertir_tablero(int MatrizEstado[SIZE][SIZE], int MatrizEstadoNuevo[SIZE][SIZE]) {
     for (int i = 0; i < SIZE; i++) {
         for (int j = 0; j < SIZE; j++) {
-            if (tablero_original[i][j] == 2)
-                tablero_convertido[i][j] = 0;
-            else if (tablero_original[i][j] == 0)
-                tablero_convertido[i][j] = -1;
+            if (MatrizEstado[i][j] == 2)
+                MatrizEstadoNuevo[i][j] = 0;  // Espacios vacíos se convierten a 0
+            else if (MatrizEstado[i][j] == 0)
+                MatrizEstadoNuevo[i][j] = -1;  // Cambia de representación 0 a -1
             else
-                tablero_convertido[i][j] = 1;
+                MatrizEstadoNuevo[i][j] = 1;  // Los demás valores se mantienen como 1
         }
     }
 }
 
-int comprueba_ganador(int tablero[SIZE][SIZE], int *empate)
- {
+int comprueba_ganador(int MatrizEstadoNuevo[SIZE][SIZE], int *empate) {
     int combinaciones[8][3][2] = {
         {{0, 0}, {0, 1}, {0, 2}}, {{1, 0}, {1, 1}, {1, 2}}, {{2, 0}, {2, 1}, {2, 2}}, // filas
         {{0, 0}, {1, 0}, {2, 0}}, {{0, 1}, {1, 1}, {2, 1}}, {{0, 2}, {1, 2}, {2, 2}}, // columnas
         {{0, 0}, {1, 1}, {2, 2}}, {{0, 2}, {1, 1}, {2, 0}}  // diagonales
     };
 
-    *empate = 1;
+    *empate = 1;  // Asume empate inicialmente
     for (int i = 0; i < 8; i++) {
         int a = combinaciones[i][0][0], b = combinaciones[i][0][1];
         int c = combinaciones[i][1][0], d = combinaciones[i][1][1];
         int e = combinaciones[i][2][0], f = combinaciones[i][2][1];
 
-        if (tablero[a][b] != 0 && tablero[a][b] == tablero[c][d] && tablero[a][b] == tablero[e][f]) {
-            return tablero[a][b];
+        if (MatrizEstadoNuevo[a][b] != 0 && MatrizEstadoNuevo[a][b] == MatrizEstadoNuevo[c][d] && MatrizEstadoNuevo[a][b] == MatrizEstadoNuevo[e][f]) {
+            return MatrizEstadoNuevo[a][b];
         }
     }
 
     for (int i = 0; i < SIZE; i++) {
         for (int j = 0; j < SIZE; j++) {
-            if (tablero[i][j] == 0) {
-                *empate = 0;
+            if (MatrizEstadoNuevo[i][j] == 0) {
+                *empate = 0;  // No es empate si hay un espacio vacío
                 return 0;
             }
         }
     }
 
-    return 0;
+    return 0;  // Si no hay ganador y no hay espacios vacíos, es empate
 }
 
-int minimax(int tablero[SIZE][SIZE], int prof, int isMax) 
-{
+int minimax(int MatrizEstadoNuevo[SIZE][SIZE], int prof, int isMax) {
     int empate;
-    int ganador = comprueba_ganador(tablero, &empate);
+    int ganador = comprueba_ganador(MatrizEstadoNuevo, &empate);
 
     if (ganador != 0) {
         return (ganador == 1 ? 100 - prof : -100 + prof);
@@ -63,10 +67,10 @@ int minimax(int tablero[SIZE][SIZE], int prof, int isMax)
         int mejorPuntuacion = INT_MIN;
         for (int i = 0; i < SIZE; i++) {
             for (int j = 0; j < SIZE; j++) {
-                if (tablero[i][j] == 0) {
-                    tablero[i][j] = 1;
-                    int puntuacion = minimax(tablero, prof + 1, 0);
-                    tablero[i][j] = 0;
+                if (MatrizEstadoNuevo[i][j] == 0) {
+                    MatrizEstadoNuevo[i][j] = 1;
+                    int puntuacion = minimax(MatrizEstadoNuevo, prof + 1, 0);
+                    MatrizEstadoNuevo[i][j] = 0;
                     if (puntuacion > mejorPuntuacion) {
                         mejorPuntuacion = puntuacion;
                     }
@@ -78,10 +82,10 @@ int minimax(int tablero[SIZE][SIZE], int prof, int isMax)
         int mejorPuntuacion = INT_MAX;
         for (int i = 0; i < SIZE; i++) {
             for (int j = 0; j < SIZE; j++) {
-                if (tablero[i][j] == 0) {
-                    tablero[i][j] = -1;
-                    int puntuacion = minimax(tablero, prof + 1, 1);
-                    tablero[i][j] = 0;
+                if (MatrizEstadoNuevo[i][j] == 0) {
+                    MatrizEstadoNuevo[i][j] = -1;
+                    int puntuacion = minimax(MatrizEstadoNuevo, prof + 1, 1);
+                    MatrizEstadoNuevo[i][j] = 0;
                     if (puntuacion < mejorPuntuacion) {
                         mejorPuntuacion = puntuacion;
                     }
@@ -92,17 +96,15 @@ int minimax(int tablero[SIZE][SIZE], int prof, int isMax)
     }
 }
 
-void mov_optimo(int tablero_convertido[SIZE][SIZE], int *fila, int *columna) 
-{
+void mov_optimo(int MatrizEstadoNuevo[SIZE][SIZE], int *fila, int *columna) {
     int mejor_puntuacion = INT_MIN;
-    int casilla_optima = -1;
 
     for (int i = 0; i < SIZE; i++) {
         for (int j = 0; j < SIZE; j++) {
-            if (tablero_convertido[i][j] == 0) {
-                tablero_convertido[i][j] = 1;
-                int puntuacion = minimax(tablero_convertido, 0, 0);
-                tablero_convertido[i][j] = 0;
+            if (MatrizEstadoNuevo[i][j] == 0) {
+                MatrizEstadoNuevo[i][j] = 1;
+                int puntuacion = minimax(MatrizEstadoNuevo, 0, 0);
+                MatrizEstadoNuevo[i][j] = 0;
 
                 if (puntuacion > mejor_puntuacion) {
                     mejor_puntuacion = puntuacion;
@@ -117,17 +119,14 @@ void mov_optimo(int tablero_convertido[SIZE][SIZE], int *fila, int *columna)
 void setup() {
     Serial.begin(115200);
 
-    int tablero_original[SIZE][SIZE] = {
-        {-1, 2, -1},
-        {2, 1, 2},
-        {2, 1, 2}
-    };
+    // Llamar a la función externa para actualizar MatrizEstado
+    Fn_MatrizState();
 
-    int tablero_convertido[SIZE][SIZE];
-    convertir_tablero(tablero_original, tablero_convertido);
+    int MatrizEstadoNuevo[SIZE][SIZE];
+    convertir_tablero(MatrizEstado, MatrizEstadoNuevo);
 
     int empate;
-    int ganador = comprueba_ganador(tablero_convertido, &empate);
+    int ganador = comprueba_ganador(MatrizEstadoNuevo, &empate);
 
     if (ganador == 1) {
         Serial.println("El robot ha ganado.");
@@ -137,10 +136,10 @@ void setup() {
         Serial.println("El juego ha terminado en empate.");
     } else {
         int fila, columna;
-        mov_optimo(tablero_convertido, &fila, &columna);
-        tablero_convertido[fila][columna] = 1;
+        mov_optimo(MatrizEstadoNuevo, &fila, &columna);
+        MatrizEstadoNuevo[fila][columna] = 1;
 
-        ganador = comprueba_ganador(tablero_convertido, &empate);
+        ganador = comprueba_ganador(MatrizEstadoNuevo, &empate);
 
         Serial.print("La máquina debe hacer su próximo movimiento en la fila ");
         Serial.print(fila);
