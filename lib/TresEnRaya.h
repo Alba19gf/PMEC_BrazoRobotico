b1264189dc3fd48b203ce67b5808b0abb49f3cc2
Alba19gf <115873114+Alba19gf@users.noreply.github.com>
2024-06-12 18:49:42 +0200

    c

--- lib/TresEnRaya.h
+++ lib/TresEnRaya.h
@@ -10,8 +10,8 @@ extern int MatrizEstado[3][3]; //MatrizEstado se declara extern para estar dispo
 void Fn_printMatriz(int MatrizEstadoNuevo[SIZE][SIZE]);
 int convertir_matrizGlobal(int MatrizEstado[SIZE][SIZE], int MatrizEstadoNuevo[SIZE][SIZE]);
 
-int comprueba_ganador(int MatrizEstadoNuevo[SIZE][SIZE], int *empate);
-int minimax(int MatrizEstadoNuevo[SIZE][SIZE], int prof, int isMax, int* resultado);
+int comprueba_ganador(int MatrizEstadoNuevo[SIZE][SIZE]);
+int minimax(int MatrizEstadoNuevo[SIZE][SIZE], int prof, bool isMax);
 int mov_optimo();
 
 int PosicionOptima();
